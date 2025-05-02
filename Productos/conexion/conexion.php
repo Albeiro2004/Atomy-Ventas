06e1cb09d6e26f5c91caadc72b4167142a86e31e
Albeiro Hoyos <AHOYOSGONZALEZ25@CORREO.UNICORDOBA.EDU.CO>
2025-05-01 21:23:41 -0500

    1era

--- Productos/conexion/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conexion = new mysqli("bp0crvzunqnpxlmelgi5-mysql.services.clever-cloud.com",
-                       "u4aoh5hhhu3ccxcd", "GvDtABczwxtqsHvvhnXl", "bp0crvzunqnpxlmelgi5");
-if ($conexion->connect_error) {
-    die("Error de conexión: " . $conexion->connect_error);
-}
-?>
